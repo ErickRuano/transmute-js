390233bd5351ec7b025e1877d4fe4c779b3e332f
Luis Gudiel <luis.gudiel@pentcloud.com>
2018-06-07 10:08:08 -0600

    add new circle and login(default user)

---
+++ circles/webv2/elements/controller.js
@@ -0,0 +1,138 @@
+app.controller('{{pluralId}}Controller', function(appConfig, $scope, toastr, API, $filter){
+    // initialization components
+    $('.dropdown-button').dropdown();
+    $('.modal').modal();
+    $('ul.tabs').tabs();
+    $('.collapsible').collapsible();
+  	// Set module title
+  	appConfig.setTitle("{{pluralId}}");
+    // Flag to show/hide loading animation
+    $scope.loading = true;
+    // Get local session
+    // $scope.user = JSON.parse(localStorage.user);
+    $scope.user = {};
+  	$scope.categories = []; // Categories collection
+    //pagination begin
+    $scope.currentPage = 0;
+    $scope.pageSize = 5;
+    $scope.pages = [];
+    $scope.showData = function () {
+      if ($scope.searchtest!=="") {
+        $scope.pageSize=$scope.{{pluralId}}.length;
+      }else{
+        $scope.pageSize=5;
+      }
+      $scope.configPages()
+    }
+    $scope.setPageSize = function (value) {
+      $scope.pageSize=value;
+      $scope.configPages()
+    }
+    $scope.configPages = function() {
+      $scope.pages.length = 0;
+      var ini = $scope.currentPage - 4;
+      var fin = $scope.currentPage + 5;
+      if (ini < 1) {
+        ini = 1;
+        if (Math.ceil($scope.{{pluralId}}.length / $scope.pageSize) > 10){
+          fin = 10;
+        }
+        else
+          fin = Math.ceil($scope.{{pluralId}}.length / $scope.pageSize);
+      } else {
+        if (ini >= Math.ceil($scope.{{pluralId}}.length / $scope.pageSize) - 10) {
+          ini = Math.ceil($scope.{{pluralId}}.length / $scope.pageSize) - 10;
+          fin = Math.ceil($scope.{{pluralId}}.length / $scope.pageSize);
+        }
+      }
+      if (ini < 1){ ini = 1; }
+      for (var i = ini; i <= fin; i++) {
+        $scope.pages.push({
+          no: i
+        });
+      }
+
+      if ($scope.currentPage >= $scope.pages.length){
+        $scope.currentPage = $scope.pages.length - 1;
+      }
+    };
+
+    $scope.setPage = function(index) {
+      $scope.currentPage = index - 1;
+    };
+    //pagination end
+    // Temporal models for CRUD
+    $scope.new{{capitalizedId}} = {};
+  	$scope.tmp{{capitalizedId}} = {};
+  	$scope.setTmp{{capitalizedId}} = function(obj, idx){
+  		clone = JSON.parse(JSON.stringify(obj));
+  		$scope.tmp{{capitalizedId}} = clone;
+      $scope.idx = idx;
+  		return clone;
+  	}; 
+
+    // Method to create a new
+    $scope.add{{capitalizedId}} = function(){
+      var valid = true;
+      // if(!$scope.newCategory.name ){ valid = false; };
+      if(valid){
+          $('#add{{capitalizedId}}Dialog').modal('close');
+        API.{{lowerId}}.create($scope.new{{capitalizedId}}).then(function(cat){
+          // $scope.{{pluralId}}.push(cat);
+          fetch();
+           toastr.success("{{capitalizedId}} agregado correctamente");
+        }, function(){
+          
+        });
+      }else{
+        toastr.error("Debes completar todos los campos");
+      };
+    };
+
+    // Method to edit a existing 
+  	$scope.edit{{capitalizedId}} = function(){
+  		var valid = true;
+  		// if(!$scope.tmpCategory.name){ valid = false; };
+  		if(valid){
+        $('#edit{{capitalizedId}}Dialog').modal('open');
+        API.{{lowerId}}.update($scope.tmp{{capitalizedId}}).then(function(){
+          fetch();
+  			   toastr.success("{{capitalizedId}} actualizado");
+        }, function(){
+          
+        });
+  		}else{
+  			toastr.error("Debes completar todos los campos");
+  		};
+  	};
+
+    // Method to delete a existing 
+  	$scope.delete{{capitalizedId}} = function(){
+        API.{{lowerId}}.delete($scope.tmp{{capitalizedId}}).then(function(){
+  		    toastr.success("{{capitalizedId}} eliminado");
+          $scope.{{pluralId}}.splice($scope.idx, 1);
+          $('#delete{{capitalizedId}}Dialog').modal('open');
+        }, function(){
+          
+        });
+  	};
+    
+    //open and close Dialogs;
+    $scope.openDialog = function (dialog) {
+      $('#'+dialog).modal('open');
+    }
+    $scope.closeDialog = function (dialog) {
+      $('#'+dialog).modal('close');
+    }
+
+    // API call to populate main collection
+    var fetch = function(){
+      API.{{lowerId}}.getAll().then(function({{pluralId}}){
+        $scope.{{pluralId}} = {{pluralId}};
+        $scope.configPages()
+        $scope.$apply();
+      });
+    };
+    fetch();
+
+});
---
+++ circles/webv2/elements/index.js
@@ -0,0 +1,111 @@
+// Production
+var API_URL = "{{configuration.protocol}}://{{configuration.remote}}:{{configuration.port}}";
+
+var app = angular.module('{{id}}', ['ui.router', 'angularLazyImg', 'angular.filter', 'toastr', 'flow', 'api-client', 'ui.materialize']);
+
+app.config(function(toastrConfig) {
+	console.log(toastrConfig);
+  angular.extend(toastrConfig, {
+    autoDismiss: false,
+    containerId: 'toast-container',
+    maxOpened: 0,    
+    newestOnTop: true,
+    positionClass: 'toast-top-center',
+    preventDuplicates: false,
+    preventOpenDuplicates: false,
+    target: 'body',
+    closeButton: true,
+    progressBar: true
+  });
+});
+
+app.config(['flowFactoryProvider', function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+        target: API_URL,
+        singleFile : true,
+        permanentErrors:[500, 501],
+        testChunks:false
+    };
+}]);
+
+app.config(['$stateProvider', '$urlRouterProvider',
+
+function config($stateProvider, $urlRouterProvider) {
+// if none of the below states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+  $urlRouterProvider.when('/app', function(){
+  	setTimeout(function(){
+  		try{
+  			user = JSON.parse(localStorage.user);
+				{{#if @last}} window.location.hash += '/{{lowerName}}';{{/if}}
+  		}catch(err){
+  			window.location.hash += '/login';
+  		};
+  	}, 1000);
+  });
+
+	var loginState = {
+		name: 'login',
+		url: '/login',
+		views : {
+			'workspace' : {
+				templateUrl: 'modules/login/template.html'
+			}
+		}
+	};
+
+	var appState = {
+		name: 'app',
+		url: '/app',
+		views : {
+			'workspace' : {
+				templateUrl: 'modules/app/menu.html'
+			}
+		}
+	};
+
+	{{#composition}}
+	var {{lowerName}}State = {
+		name: 'app.{{lowerName}}',
+		url: '/{{lowerName}}',
+		views : {
+			'module' : {
+				templateUrl: 'modules/{{lowerName}}/template.html'
+			}
+		}
+	};
+	{{/composition}}
+
+
+	$stateProvider.state(loginState);
+	$stateProvider.state(appState);
+	{{#composition}}
+	$stateProvider.state({{lowerName}}State);
+	{{/composition}}
+
+}
+]);
+	
+
+app.factory('appConfig', function($rootScope) {
+	return {
+		setTitle: function(title){
+			$rootScope.title = title;
+		},
+		setAPI: function(API_URL){
+			$rootScope.API_URL = API_URL;
+		}
+	}
+});
+
+app.filter('startFromGrid', function() {
+	return function(input, start) {
+		start = +start;
+		try{
+			return input.slice(start);
+		}catch(e){
+			
+		}
+	}
+});
